d9928a6f1bbb856dab35abc6a78e9100fbb264da
raaz-srijan <srijannospace@gmail.com>
2026-05-14 22:50:08 +0545

    added a global error handler

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import express from "express";
+import { ENV } from "./infrastructure/env.js";
+import { connectDb } from "./infrastructure/connectDb.js";
+import connectCloudinary from "./infrastructure/connectCloudinary.js";
+import { globalErrorHandler } from "./shared/middlewares/error.middleware.js";
+
+import * as route from "./routes/routes.js";
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = ENV.PORT;
-
-import { ENV } from "./infrastructure/env.js";
-import { connectDb } from "./infrastructure/connectDb.js";
-import connectCloudinary from "./infrastructure/connectCloudinary.js";
-
-import * as route from "./routes/routes.js";
 
 app.use("/api/v1/user", route.userRoute);
 app.use("/api/v1/permissions", route.permissionRoute);
@@ -20,8 +20,10 @@ app.use("/api/v1/authors", route.authorRoute);
 app.use("/api/v1/category", route.categoryRoute);
 app.use("/api/v1/genre", route.genreRoute);
 
+app.use(globalErrorHandler);
+
 app.listen(PORT, ()=> {
     console.log(`Server started on ${PORT}`);
     connectDb();
     connectCloudinary();
-})
+});
