7d0d65c51290cda700d79facebea8d25005242b3
raaz-srijan <srijannospace@gmail.com>
2026-05-14 22:46:35 +0545

    added a error helper

--- modules/user/user.controller.ts
+++ modules/user/user.controller.ts
@@ -1,21 +1,19 @@
 import type { Request, Response } from "express";
 import { UserService } from "./user.service.js";
-
+import { catchAsync } from "../../shared/errors/catchAsync.js";
 
 export class UserController {
-    static async register(req:Request, res:Response) {
+    static register = catchAsync(async (req: Request, res: Response) => {
         const user = await UserService.createUser(req.body);
         return res.status(201).json({success:true, message:"User registered successfully", data:user});
-    }
+    });
 
-    static async update(req:Request, res:Response) {
+    static update = catchAsync(async (req: Request, res: Response) => {
         if(!req.user)
             return res.status(403).json({success:false, message:"Please login"});
 
         const userId = req.user._id.toString();
         const user = await UserService.updateUser(userId, req.body);
         return res.status(200).json({success:true, message:"User updated successfully", data:user});
-    }
-
-    
+    });
 }
--- modules/user/user.route.ts
+++ modules/user/user.route.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
 import { UserController } from "./user.controller.js";
+import { protect } from "../../shared/middlewares/auth.middleware.js";
 
 const route = Router();
 
 route.post("/register", UserController.register);
-route.patch("/update", UserController.update);
+route.patch("/update", protect, UserController.update);
 
 export default route;
