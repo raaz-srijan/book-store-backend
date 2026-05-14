ad7374a30cfff62c9ddbb69d3d01c89adf74e9d0
raaz-srijan <srijannospace@gmail.com>
2026-05-14 22:43:07 +0545

    added a request controller

--- modules/vendor/vendor.controller.ts
+++ modules/vendor/vendor.controller.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
 import { VendorService } from "./vendor.service.js";
+import { catchAsync } from "../../shared/errors/catchAsync.js";
 
 export class VendorController {
-    static async requestVendor(req:Request, res:Response) {
-
+    static requestVendor = catchAsync(async (req: Request, res: Response) => {
         if(!req.user)
             return res.status(403).json({success:false, message:"Please login"});
 
@@ -12,11 +12,10 @@ export class VendorController {
         const vendor = await VendorService.requestVendor(userId, req.body)
 
         return res.status(201).json({success:true, message:"Request for vendor successfully", data:vendor});
-    }
+    });
 
-    static async approveVendorRequest(req:Request, res:Response) {
-
-        const id = req.params;
+    static approveVendorRequest = catchAsync(async (req: Request, res: Response) => {
+        const { id } = req.params;
 
         if(!id)
             return res.status(404).json({success:false, message:"Invalid Id"});
@@ -24,30 +23,32 @@ export class VendorController {
         const approve = await VendorService.ApproveVendorRequest(id.toString());
 
         return res.status(200).json({success:true, message:"Request approved successfully", data:approve});
-    }
+    });
 
-    static async resjectVendorRequest(req:Request, res:Response) {
-        const id = req.params;
+    static resjectVendorRequest = catchAsync(async (req: Request, res: Response) => {
+        const { id } = req.params;
         if(!id)
             return res.status(404).json({success:false, message:"Invalid Id"});
 
         const reject = await VendorService.RejectVendorRequest(id.toString());
 
         return res.status(200).json({success:true, message:"Request rejected successfully", data:reject});
-    }
+    });
 
-    static async getAllVendorRequest(req:Request, res:Response) {
+    static getAllVendorRequest = catchAsync(async (req: Request, res: Response) => {
         const requests = await VendorService.getVendorRequest();
 
         return res.status(200).json({success:true, message:"Vendor requests fetched successfully", data:requests});
-    }
+    });
 
-    static async getVendorRequestById(req:Request, res:Response) {
+    static getVendorRequestById = catchAsync(async (req: Request, res: Response) => {
+        const { id } = req.params;
 
-        const id = req.params;
+        if(!id)
+            return res.status(404).json({success:false, message:"Invalid Id"});
 
         const request = await VendorService.getVendorRequestById(id.toString())
 
         return res.status(200).json({success:true, message:"Vendor request fetched successfully", data:request});
-    }
+    });
 }
